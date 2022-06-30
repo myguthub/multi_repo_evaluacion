e160b49f3ab60f6c0c9cab5bc99c4635005c4b7e
myguthub <gonzalo.miguel.talavera@gmail.com>
2022-06-29 21:08:26 -0400

    rest api

--- src/web/api/urls.py
+++ src/web/api/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.getData),
+    path('add/', views.addItem),
+    path('update/', views.updateItem),
+    path('delete/', views.deleteItem),
 ]
